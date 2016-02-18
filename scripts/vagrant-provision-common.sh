113020975f614c049a679d50e550893c8dd392cb
jklahn <jklahn@users.noreply.github.com>
2016-02-18 13:28:00 -0500

    Updated Docker/Docker Compose Script
    Updated Docker and Docker Compose Script per Docker Documentation.

--- scripts/vagrant-provision-common.sh
+++ scripts/vagrant-provision-common.sh
@@ -15,10 +15,10 @@ echo 127.0.0.1 sandbox.bigfix.com >> /etc/hosts
 #yum -y update
 
 # If docker's not alrady insatlled run docker install script
-# see https://docs.docker.com/docker/installation/centos/
+# see https://docs.docker.com/engine/installation/linux/centos/
 if [ ! -f /etc/systemd/system/docker.service.d/docker.conf ]
 then
-  curl -sSL https://get.docker.com/ | sh
+  curl -fsSL https://get.docker.com/ | sh
 
   # change the docker config to allocate 20G to containers
   # see https://docs.docker.com/articles/systemd/ for info on configuration
@@ -36,8 +36,6 @@ systemctl enable docker
 # install docker compose if it's not already there
 if [ ! -f /usr/local/bin/docker-compose ]
 then
-  curl -L \
-    https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` \
-    > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 fi
